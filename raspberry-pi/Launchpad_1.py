ab3edd15eee65d3524bae5ac9c15a68a1564b82e
Jweder06 <jweder06@charlottesvilleschools.org>
2023-09-08 10:36:03 -0400

    Finished launchpad 1

--- raspberry-pi/Launchpad_1.py
+++ raspberry-pi/Launchpad_1.py
@@ -3,5 +3,15 @@ import time
 import board
 import digitalio
 
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
+led = digitalio.DigitalInOut(board.GP1)
+led.direction = digitalio.Direction.OUTPUT 
+
+Count_time = 11
+ 
+while True:
+    if Count_time >= 1:
+        Count_time = Count_time - 1
+        time.sleep(1)
+        print(Count_time)
+        if Count_time == 0:
+            print("Launch")
