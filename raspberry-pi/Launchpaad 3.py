1e336c4fb4e76c3a04e147b84e972dd5928aeba6
Jweder06 <jweder06@charlottesvilleschools.org>
2023-09-13 10:48:59 -0400

    spicy launchpad 3

--- raspberry-pi/Launchpaad 3.py
+++ raspberry-pi/Launchpaad 3.py
@@ -11,23 +11,29 @@ Button = digitalio.DigitalInOut(board.GP1)
 Button.pull = digitalio.Pull.DOWN
 Count_time = 11
 Button_state = 0
+Start_value = 0
 while True:
     if Button.value == True and Button_state == 0:
-        print(Button_state)
         Button_state = 1
-    elif Button.value == False:
+        Start_value = Start_value + 1
+    if Button.value == False:
         Button_state = 0
-    if Count_time >= 1 and Button_state == 1:
-        Gled.value = False
-        Count_time = Count_time - 1
-        Rled.value = True
-        time.sleep(.5)
-        Rled.value = False
-        time.sleep(.5)
-        print(Count_time)
+    if Count_time >= 1 and Start_value == 1:
+         Gled.value = False
+         Count_time = Count_time - 1
+         Rled.value = True
+         time.sleep(.5)
+         Rled.value = False
+         time.sleep(.5)
+         print(Count_time)
     if Count_time == 0:
-        print("Launch")
-        Gled.value = True
-        Rled.value = False
-        time.sleep(5)
+         print("Launch")
+         Gled.value = True
+         Rled.value = False
+         time.sleep(5)
+         Count_time = 11
+    if   Start_value == 2:
+        print("abort")
+    if Start_value == 3:
+        Start_value = 1
         Count_time = 11
